03939c78b9d22e2345ed9f77b71ed22a07b6dfcc
eddieor <eortizr06@gmail.com>
2023-07-08 19:37:26 -0700

    Number display complete

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@ const erase = document.getElementById('delete');
 const numberButtons = document.querySelectorAll('[data-number]');
 
 let firstNumber = '';
-let operand = null;
+let currentOperand = null;
 let secondNumber = '';
 let resetScreen = false ;
 
@@ -20,10 +20,10 @@ window.addEventListener('keydown', handleKeyboardInput);
 
 numberButtons.forEach((button) =>
   button.addEventListener('click', () => appendNumber(button.textContent))
-);
+  );
 
 function appendNumber(number) {
-    if (operand === null) {
+    if (currentOperand === null) {
       firstNumber += number;
       currentOperation.textContent = `${firstNumber}`;
     } else {
@@ -32,21 +32,21 @@ function appendNumber(number) {
     }
   }
 
-function add(a, b) {
-    return a + b
-  }
+    function add(a, b) {
+        return a + b
+    }
   
-  function substract(a, b) {
-    return a - b
-  }
+    function substract(a, b) {
+        return a - b
+    }
   
-  function multiply(a, b) {
-    return a * b
-  }
+    function multiply(a, b) {
+        return a * b
+    }
   
-  function divide(a, b) {
-    return a / b
-  }
+    function divide(a, b) {
+        return a / b
+    }
 
   function handleKeyboardInput(e){
     if(e.key >= 0 && e.key <= 9) appendNumber(e.key)
@@ -57,18 +57,31 @@ function add(a, b) {
     if(e.key === '/' || e.key === '+' || e.key === '*' || e.key ==='-') setOperation(appendOperand(e.key))
   }
 
-  appendOperand(operand){
+  /*appendOperand(operand){
+    
+  }*/
 
+  function setOperation(operand) {
+    switch (operand) {
+      case 'x':
+        multiply(firstNumber, secondNumber);
+        result.textContent = multiply(firstNumber, secondNumber);
+        break;
+      case '+':
+        add(firstNumber, secondNumber);
+        result.textContent = add(firstNumber, secondNumber);
+        break;
+      // Add more cases for other operations if needed
+      default:
+        // Handle unrecognized operand
+        break;
+    }
   }
 
-  setOperation(operand){
-
-    if (operand == "+") 
-  }
-
-clear.addEventListener('click', handleClearClick);
-erase.addEventListener('click', handleDeleteClick);
+//clear.addEventListener('click', handleClearClick);
+//erase.addEventListener('click', handleDeleteClick);
 plus.addEventListener('click', () => setOperation(plus.textContent));
 minus.addEventListener('click', () => setOperation(minus.textContent));
 divide.addEventListener('click', () => setOperation(divide.textContent));
 times.addEventListener('click', () => setOperation(times.textContent));
+numberButtons.addEventListener('click', appendNumber(numberButtons.textContent));
