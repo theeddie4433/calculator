f9fd1a63d0140ccaf85cc6fac667d5b29a3fd6eb
eddieor <eortizr06@gmail.com>
2023-07-10 19:32:23 -0700

    setOperation bug fixed, only function pending pointAppend

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ const plusButton = document.getElementById('plus');
 const minusButton = document.getElementById('minus');
 const timesButton = document.getElementById('times');
 const divideButton = document.getElementById('divided');
+const equalButton = document.getElementById('equal');
 const currentOperationDisplay = document.querySelector('.currentOp');
 const resultDisplay = document.querySelector('.result');
 const clearButton = document.getElementById('clear');
@@ -21,7 +22,7 @@ numberButtons.forEach((button) => {
 
 function appendNumber(number) {
     if (currentOperationDisplay.textContent === '0' || shouldResetScreen)
-    resetScreen()
+      resetScreen()
     currentOperationDisplay.textContent += number
   }
 
@@ -32,7 +33,7 @@ function erase(){
 }
 
 function clear(){
-  currentOperationDisplay.textContent = '';
+  currentOperationDisplay.textContent = '0';
   resultDisplay.textContent = '';
   firstNumber = '';
   currentOperation = null;
@@ -72,28 +73,32 @@ function divide(a, b) {
   }
 
 function setOperation(operand) {
-  if (currentOperation !== null){
-  evaluate()
-  }else{
+  if (currentOperation !== null) evaluate()
   firstNumber = currentOperationDisplay.textContent
   currentOperation = operand
   resultDisplay.textContent = `${firstNumber} ${currentOperation}`
-  currentOperationDisplay.textContent = ""
   shouldResetScreen = true
+  console.log(firstNumber)
+  console.log(secondNumber)
   }
-}
+
 
 function evaluate(){
-  secondNumber = currentOperationDisplay.textContent
+  if (currentOperation === null || shouldResetScreen) return
   if(firstNumber === 0 && currentOperation === '÷'){
     currentOperationDisplay = 'ERROR' 
     resultDisplay = ''
-  } 
+    return
+  }
+  secondNumber = currentOperationDisplay.textContent
   currentOperationDisplay.textContent = roundResult(
     operate(currentOperation, firstNumber, secondNumber)
   )
-  resultDisplay.textContent = `${firstNumber} ${currentOperation} ${secondNumber}`
-  resetScreen = true;
+  resultDisplay.textContent = `${firstNumber} ${currentOperation} ${secondNumber} =` 
+  console.log(firstNumber)
+  console.log(secondNumber)
+  currentOperation = null
+  console.log(currentOperation)
 }
 
 function operate(operand, a, b){
@@ -101,13 +106,21 @@ function operate(operand, a, b){
   b = Number(b)
   switch(operand){
     case '+':
-      return add(a,b);
+      return add(a , b);
+    case '-':
+      return substract(a , b);
+    case '×':
+      return multiply(a , b);
+    case '÷':
+      return divide(a , b);
+
   }
 }
 function roundResult(number) {
   return Math.round(number * 1000) / 1000
 }
 
+equalButton.addEventListener('click', () => evaluate())
 clearButton.addEventListener('click', clear);
 eraseButton.addEventListener('click', erase);
 plusButton.addEventListener('click', function () {
@@ -120,6 +133,5 @@ plusButton.addEventListener('click', function () {
     setOperation('÷');
   });
   timesButton.addEventListener('click', function () {
-    setOperation('x');
+    setOperation('×');
   });
-numberButtons.addEventListener('click', () => appendNumber(numberButtons.textContent));
